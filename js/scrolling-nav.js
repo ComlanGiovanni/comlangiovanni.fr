973de1ffb518192f523949c890b3ab84eb66e1aa
ComlanGiovanni <comlan.giovanni@yahoo.fr>
2016-04-29 10:53:05 +0200

    efqc

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,9 +1,9 @@
 //jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-bottom").addClass("top-nav-collapse");
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
-        $(".navbar-fixed-bottom").removeClass("top-nav-collapse");
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 });
 
